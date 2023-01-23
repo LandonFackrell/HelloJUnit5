545f0a65d02b66fea5694a807ad69ec57450a075
Landon Fackrell <landonfackrell12@gmail.com>
2023-01-23 10:09:15 -0700

    Add tests for positive and negative longs
    Tests adding in Junit5AppTest.java

--- src/test/java/com/makotojava/learn/hellojunit5/JUnit5AppTest.java
+++ src/test/java/com/makotojava/learn/hellojunit5/JUnit5AppTest.java
@@ -4,10 +4,12 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
@@ -30,22 +32,35 @@ import org.slf4j.LoggerFactory;
 public class JUnit5AppTest {
 
   // Create a JDK Logger here
+  Logger logger = LoggerFactory.getLogger(getClass());
 
   // Create a fixture for the class under test
+  App app;
 
   // Do something before ANY test is run in this class
+  @BeforeAll
   public static void init() {
   }
 
   // Do something after ALL tests in this class are run
+  @AfterAll
   public static void done() {
   }
 
   // Create an instance of the test class before each @Test method is executed
+  @BeforeEach
+  void beforeEach(){
+    app = new App();
+  }
 
   // Destroy reference to the instance of the test class after each @Test method is executed
+  @AfterEach
+  void afterEach(){
+    app = null;
+  }
 
   // Disabled test
+  @Disabled
   void testNotRun() {
   }
 
@@ -66,12 +81,24 @@ public class JUnit5AppTest {
    * {@link org.junit.jupiter.api.Assertions#assertAll(org.junit.jupiter.api.function.Executable...) assertAll()}
    * </ol>
    */
+  @Test
+  @DisplayName("Test the Add function of positive longs in App.java")
   public void testAdd() {
-    //
-    // EXERCISE: TODO: ADD CODE HERE (See Javadoc comments for instructions. Use the Javadoc View in Eclipse to see the
-    // buttery smooth javadoc above.)
-    //
-    fail("Test not implemented!");
+    Assertions.assertNotNull(app);
+
+    long[] arr1 = new long[]{1, 2, 3, 4};
+    long[] arr2 = new long[]{20, 934, 110};
+    long[] arr3 = new long[]{2, 4, 6};
+
+    logger.info("Constructed arrays of positive longs");
+    
+    Assertions.assertAll(
+      () -> Assertions.assertEquals(10, app.add(arr1), "The sum of 1, 2, 3, and 4 should be 10"),
+      () -> Assertions.assertEquals(1064, app.add(arr2), "The sum of 20, 934, and 110 should be 1064"),
+      () -> Assertions.assertEquals(12, app.add(arr3), "The sum of 2, 4, and 6 should be 12")
+    );
+
+    logger.info("All assertions passed for adding positive long arrays");
   }
 
   /**
@@ -92,7 +119,21 @@ public class JUnit5AppTest {
    * </ol>
    * 
    */
+  @Nested
+  @DisplayName("Test the proper summing of negative longs in App.add()")
   class NegativeNumbersTest {
+
+    App app;
+
+    @BeforeEach
+    void setup(){
+      app = new App();
+    }
+
+    @AfterEach
+    void tearDown(){
+      app = null;
+    }
 
     /**
      * testAdd() - Exercises:
@@ -112,12 +153,24 @@ public class JUnit5AppTest {
      * {@link org.junit.jupiter.api.Assertions#assertAll(org.junit.jupiter.api.function.Executable...) assertAll()}
      * </ol>
      */
+    @Test
+    @DisplayName("Test summing of long arrays including negative integers")
     public void testAdd() {
-      //
-      // EXERCISE: TODO: ADD CODE HERE (See Javadoc comments for instructions. Use the Javadoc View in Eclipse to see
-      // the buttery smooth javadoc above.)
-      //
-      fail("Test not implemented!");
+      Assertions.assertNotNull(app);
+
+      long[] arr1 = new long[]{1, -2, -3, 4};
+      long[] arr2 = new long[]{20, -934, -110};
+      long[] arr3 = new long[]{-2, -4, -6};
+
+      logger.info("Constructed arrays of mixed positive and negative longs");
+      
+      Assertions.assertAll(
+        () -> Assertions.assertEquals(0, app.add(arr1), "The sum of 1, -2, -3, and 4 should be 0"),
+        () -> Assertions.assertEquals(-1024, app.add(arr2), "The sum of 20, -934, and -110 should be -1024"),
+        () -> Assertions.assertEquals(-12, app.add(arr3), "The sum of -2, -4, and -6 should be -12")
+      );
+
+      logger.info("All assertions passed for adding positive and negative long arrays");
     }
   }
 
